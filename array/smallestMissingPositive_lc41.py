7ba342c9b6218082028af781751b8cf4a6a0bda3
Mihir Ochani <mihir.ochani@gmail.com>
2022-07-22 00:11:34 -0400

    week2

--- array/smallestMissingPositive_lc41.py
+++ array/smallestMissingPositive_lc41.py
@@ -3,12 +3,19 @@ def smallestMissingPositive(nums):
 
     if nums is None or min(nums) > currentSmallestPositive:
         return 1
-    currentSmallestPositive = 1
+  
     for num in nums:
         if(num ==currentSmallestPositive):
             currentSmallestPositive += 1
     return currentSmallestPositive
 
 #doubt - solution follows constraint although is inefficient for larger lists, if take hashset of nums does it count as extra space ?
+1 -> n + 1 if numbers are there from 1 to n solution n + 1
+check only for possible solutions 
+eliminate the negatives 
+eliminate the numbers that are greater than length of array 
 
-#smallestMissingPositive([7,8,9,11,12])
+[3,4,-1,0]  n^2 we don't want cannot search for range one by one 
+
+
+#smallestMissingPositive([1,7,8,9,11,12])
