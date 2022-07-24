a9fafc5690e722bd3b95e58fb07fc6b8b3014186
Mihir Ochani <mihir.ochani@gmail.com>
2022-07-24 02:16:17 -0400

    type

---
+++ general/pythonTypicalConstructs.py
@@ -0,0 +1,13 @@
+#this file show cases tricks in python to implement code efficiently
+
+#reverse loop - last element to zero in reverse order 
+
+for(i in range(nums)-1 , 0 , -1):
+    print(nums[i])
+    result.append(nums[i]) #  appended reverse of nums in result 
+
+#dictionary iterate and access keys and values 
+freq = {}
+for value, count in freq.items():
+    result.append(count)
+
