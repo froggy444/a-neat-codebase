608a90204bda90e61a3169c795176169a15516ca
Mihir Ochani <mihir.ochani@gmail.com>
2022-07-25 12:55:07 -0400

    koko

--- general/pythonTypicalConstructs.py
+++ general/pythonTypicalConstructs.py
@@ -11,3 +11,11 @@ freq = {}
 for value, count in freq.items():
     result.append(count)
 
+#jump table
+operators = {'+': lambda y, x: x + y, '-': lambda y, x: x - y, '*': lambda y, x: x * y, '/': lambda y, x: int(x/y)  # IIFE and JUMP table concept for lambda functions in python          
+        }
+#accessing jump table and performing the lambda operation on popped elements of a stack
+        for token in tokens:
+            if token in operators:
+                stack.append(operators[token](stack.pop(), stack.pop())) # the stack.pop() basically are args y, x for the lambda jump table 
+
--- searching/kokoEatingBananas_lc875.py
+++ searching/kokoEatingBananas_lc875.py
@@ -33,6 +33,55 @@ class Solution:
                  
             else:
                 left = mid + 1 
+
+
+                ###############################################################
+                class Solution:
+    def getComputedHours(self, k: int, piles: List[int], h: int) -> int:
+        computedHours = 0
+        for pile in piles:
+            if(pile <= k):
+                #if pile == piles[-1] and pile ==k:
+                    #break
+                computedHours += 1
+               # print ('loop1:', computedHours)
+            else:
+                computedHours += math.ceil(pile/k)
+               # print ('loop2:', computedHours)
+               # print ('computedHours:', computedHours)
+        return computedHours
+    
+
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+
+        s= Solution()
+        
+        #print(eatingSpeed)
+        left = 1 
+        right = max(piles)+ 1
+       
+       # print(s.getComputedHours(1,piles,h))
+        while (left < right):
+            mid = (left + right)//2
+           # print('mid:', mid)
+           # print('len of eatingSpeed',len(eatingSpeed))
+           # print('eatingSpeed[mid]:', eatingSpeed[mid])
+            if s.getComputedHours(mid, piles, h) <= h:
+                #return eatingSpeed[mid]
+
+            #elif s.getComputedHours(eatingSpeed[mid], piles, h) < h:
+                right = mid
+                
+                 
+            else:
+                left = mid + 1 
+        
+    
+        return left
+        
+        
+        
+            
     
     
     
