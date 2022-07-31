5a7ab0644813f2cf4028a819d262197e0843d193
Mihir Ochani <mihir.ochani@gmail.com>
2022-07-31 13:25:59 -0400

    levelorder or bfs

---
+++ binaryTree/levelOrderTraversalBFS_lc102.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        # O(n) Space and Time -> space is n due to queue of all elements eventually 
+        
+        res =[]
+        q = collections.deque()
+        q.append(root)
+        
+        while q:
+            qLen = len(q)
+            print(qLen)
+            level = []
+            for i in range(qLen):
+                node = q.popleft()
+                if node:
+                    level.append(node.val)
+                    q.append(node.left)
+                    q.append(node.right)
+            if level:
+                res.append(level)
+        return res
+        
